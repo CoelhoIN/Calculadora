c0b1deeddf34400fbc31db3dd364efe4e8d28d2a
Isadora <coelhooisadora@gmail.com>
2022-04-01 09:05:46 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,15 @@
 var somar = require("./módulos/somar");
 
 console.log(somar(20,90));
+
+var subtrair = require("./módulos/subtrair");
+
+console.log(subtrair(90,20));
+
+var multiplicar = require("./módulos/multiplicar");
+
+console.log(multiplicar(20,5));
+
+var dividir = require("./módulos/dividir");
+
+console.log(dividir(25,5));
